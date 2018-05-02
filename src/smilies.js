4aff8ddd12eed29b0333d4c42c5bff9b7025ea81
Evgenia Polozova <eugpoloz@gmail.com>
2018-05-02 23:48:50 +0300

    add preventDefaults, remove var from babel config

--- src/smilies.js
+++ src/smilies.js
@@ -1,9 +1,6 @@
 // @flow
 "use strict";
 
-// global vars
-declare var FORUM: any;
-
 // type defs
 type Pack = {
   smilies: Array<string>,
@@ -14,17 +11,43 @@ type AppendSmilies = Array<Pack>;
 
 // our func
 async function appendSmilies(smiliesPacks: AppendSmilies) {
-  const packsContainer = document.getElementById("smile-packs");
   const buttonsContainer = document.getElementById("smile-btns");
+  const packsContainer = document.getElementById("smile-packs");
   const smiliesContainer = document.getElementById("smile-container");
+  const closeContainerBtn = document.getElementById("smile-container-close");
 
-  function handleButtonClick(
-    { currentTarget }: MouseEvent,
-    smilies: Array<string>
-  ) {
+  const activeClass = "smilies-button--active";
+
+  function resetActiveClass() {
+    if (buttonsContainer instanceof HTMLElement) {
+      buttonsContainer
+        .querySelectorAll("a")
+        .forEach(btn => btn.classList.remove(activeClass));
+    }
+  }
+
+  function handleButtonClick(e: MouseEvent, smiliesMap: Array<string>) {
+    e.preventDefault();
+    const { currentTarget } = e;
     if (currentTarget instanceof HTMLAnchorElement) {
-      smilies.map((smile: string) => {});
-      console.log(smilies);
+      resetActiveClass();
+      currentTarget.classList.add(activeClass);
+
+      if (buttonsContainer instanceof HTMLElement) {
+        buttonsContainer.innerHTML = smiliesMap.join(" ");
+
+        if (packsContainer instanceof HTMLElement) {
+          packsContainer.style.display = "block";
+        }
+      }
+    }
+  }
+
+  function handleCloseClick(e: MouseEvent) {
+    e.preventDefault();
+    resetActiveClass();
+    if (packsContainer instanceof HTMLElement) {
+      packsContainer.style.display = "none";
     }
   }
 
@@ -34,9 +57,18 @@ async function appendSmilies(smiliesPacks: AppendSmilies) {
     buttonEl.className = "smile_custom_btn";
     buttonEl.innerText = button_text;
 
+    const smiliesMap = smilies.map(
+      (smile: string, idx: number) =>
+        `<a onclick="insert('[img]' + ${smile} +'[/img]')">
+          <img src="${smile}" alt="${button_text}${idx.toString(10)}"/>
+        </a>`
+    );
+
+    console.log(smiliesMap);
+
     // bind event listener
     buttonEl.addEventListener("click", (e: MouseEvent) =>
-      handleButtonClick(e, smilies)
+      handleButtonClick(e, smiliesMap)
     );
 
     // add it if we have a container for it, add it
@@ -44,68 +76,73 @@ async function appendSmilies(smiliesPacks: AppendSmilies) {
       buttonsContainer.insertBefore(buttonEl, null);
     }
   });
+
+  if (closeContainerBtn instanceof HTMLAnchorElement) {
+    closeContainerBtn.addEventListener("click", handleCloseClick);
+  }
 }
 
-// appendSmilies([
-//   {
-//     smilies: [
-//       "http://s2.uploads.ru/xYUaQ.gif",
-//       "http://funkyimg.com/i/21Qek.gif",
-//       "http://funkyimg.com/i/21Qeo.gif",
-//       "http://s5.uploads.ru/LiURd.gif",
-//       "http://s3.uploads.ru/XriNH.gif",
-//       "http://funkyimg.com/i/21JPX.gif",
-//       "http://funkyimg.com/i/21JY6.gif",
-//       "http://funkyimg.com/i/21Wcf.gif",
-//       "http://s1.uploads.ru/i/dskb4.gif",
-//       "http://s2.uploads.ru/YW2XL.gif",
-//       "http://f6.s.qip.ru/UdTRA7hc.gif",
-//       "http://funkyimg.com/i/21UPB.gif",
-//       "http://funkyimg.com/i/21UPC.gif",
-//       "http://funkyimg.com/i/22G1h.gif",
-//       "http://funkyimg.com/i/22G1G.gif",
-//       "http://sh.uploads.ru/G2Zgt.gif",
-//       "http://s2.uploads.ru/MmSWc.gif",
-//       "http://s2.uploads.ru/m5B3R.gif",
-//       "http://s5.uploads.ru/StdHU.gif",
-//       "http://s4.uploads.ru/t/8EBcZ.gif",
-//       "http://f6.s.qip.ru/hBxMrtm.gif",
-//       "http://f6.s.qip.ru/UdTRA7ic.gif",
-//       "http://f6.s.qip.ru/RnQpPjx0.gif",
-//       "http://s4.uploads.ru/k9vzZ.gif",
-//       "http://sh.uploads.ru/gSpMm.gif",
-//       "http://s5.uploads.ru/t/cLspr.gif",
-//       "http://f6.s.qip.ru/UdTRA7hu.gif",
-//       "http://s2.uploads.ru/xNbpK.gif",
-//       "http://s7.uploads.ru/mGULe.gif",
-//       "http://sh.uploads.ru/rYfaU.gif",
-//       "http://s5.uploads.ru/JgPrK.gif",
-//       "http://s4.uploads.ru/BslHI.gif",
-//       "http://funkyimg.com/i/21Qfa.gif",
-//       " http://funkyimg.com/i/21Qfe.gif",
-//       " http://funkyimg.com/i/22FZd.gif",
-//       " http://funkyimg.com/i/22FZc.gif",
-//       " http://s2.uploads.ru/zA2Ps.gif",
-//       "http://funkyimg.com/i/22FZT.gif",
-//       " http://funkyimg.com/i/21Qfm.gif",
-//       "http://funkyimg.com/i/21Qfk.gif",
-//       "http://sh.uploads.ru/ozYQG.gif",
-//       "http://sg.uploads.ru/tpnPL.gif",
-//       "http://funkyimg.com/i/21WcE.gif",
-//       "http://funkyimg.com/i/21WcL.gif",
-//       "http://funkyimg.com/i/22G1j.gif",
-//       "http://funkyimg.com/i/21Qfz.gif",
-//       "http://sg.uploads.ru/vgUEm.gif",
-//       "http://s3.uploads.ru/Rq2KN.gif",
-//       "http://s2.uploads.ru/CEQSw.gif",
-//       "http://funkyimg.com/i/Nhoo.gif",
-//       "http://f6.s.qip.ru/UdTRA7k2.gif",
-//       "http://pix.academ.org/img/2012/10/11/fe9f25e42a03052ce3786644cbd2c1c2.gif",
-//       "http://sh.uploads.ru/l3O1d.gif",
-//       "http://s1.uploads.ru/t/dU7eG.gif",
-//       "http://s1.uploads.ru/i/6yOkz.gif",
-//       "http://sg.uploads.ru/S9GsI.gif"
-//     ],
-//     button_text: "Гифкосмайлы"
-//   }
-// ]);
+// configuration example:
+appendSmilies([
+  {
+    smilies: [
+      "http://s2.uploads.ru/xYUaQ.gif",
+      "http://funkyimg.com/i/21Qek.gif",
+      "http://funkyimg.com/i/21Qeo.gif",
+      "http://s5.uploads.ru/LiURd.gif",
+      "http://s3.uploads.ru/XriNH.gif",
+      "http://funkyimg.com/i/21JPX.gif",
+      "http://funkyimg.com/i/21JY6.gif",
+      "http://funkyimg.com/i/21Wcf.gif",
+      "http://s1.uploads.ru/i/dskb4.gif",
+      "http://s2.uploads.ru/YW2XL.gif",
+      "http://f6.s.qip.ru/UdTRA7hc.gif",
+      "http://funkyimg.com/i/21UPB.gif",
+      "http://funkyimg.com/i/21UPC.gif",
+      "http://funkyimg.com/i/22G1h.gif",
+      "http://funkyimg.com/i/22G1G.gif",
+      "http://sh.uploads.ru/G2Zgt.gif",
+      "http://s2.uploads.ru/MmSWc.gif",
+      "http://s2.uploads.ru/m5B3R.gif",
+      "http://s5.uploads.ru/StdHU.gif",
+      "http://s4.uploads.ru/t/8EBcZ.gif",
+      "http://f6.s.qip.ru/hBxMrtm.gif",
+      "http://f6.s.qip.ru/UdTRA7ic.gif",
+      "http://f6.s.qip.ru/RnQpPjx0.gif",
+      "http://s4.uploads.ru/k9vzZ.gif",
+      "http://sh.uploads.ru/gSpMm.gif",
+      "http://s5.uploads.ru/t/cLspr.gif",
+      "http://f6.s.qip.ru/UdTRA7hu.gif",
+      "http://s2.uploads.ru/xNbpK.gif",
+      "http://s7.uploads.ru/mGULe.gif",
+      "http://sh.uploads.ru/rYfaU.gif",
+      "http://s5.uploads.ru/JgPrK.gif",
+      "http://s4.uploads.ru/BslHI.gif",
+      "http://funkyimg.com/i/21Qfa.gif",
+      " http://funkyimg.com/i/21Qfe.gif",
+      " http://funkyimg.com/i/22FZd.gif",
+      " http://funkyimg.com/i/22FZc.gif",
+      " http://s2.uploads.ru/zA2Ps.gif",
+      "http://funkyimg.com/i/22FZT.gif",
+      " http://funkyimg.com/i/21Qfm.gif",
+      "http://funkyimg.com/i/21Qfk.gif",
+      "http://sh.uploads.ru/ozYQG.gif",
+      "http://sg.uploads.ru/tpnPL.gif",
+      "http://funkyimg.com/i/21WcE.gif",
+      "http://funkyimg.com/i/21WcL.gif",
+      "http://funkyimg.com/i/22G1j.gif",
+      "http://funkyimg.com/i/21Qfz.gif",
+      "http://sg.uploads.ru/vgUEm.gif",
+      "http://s3.uploads.ru/Rq2KN.gif",
+      "http://s2.uploads.ru/CEQSw.gif",
+      "http://funkyimg.com/i/Nhoo.gif",
+      "http://f6.s.qip.ru/UdTRA7k2.gif",
+      "http://pix.academ.org/img/2012/10/11/fe9f25e42a03052ce3786644cbd2c1c2.gif",
+      "http://sh.uploads.ru/l3O1d.gif",
+      "http://s1.uploads.ru/t/dU7eG.gif",
+      "http://s1.uploads.ru/i/6yOkz.gif",
+      "http://sg.uploads.ru/S9GsI.gif"
+    ],
+    button_text: "Гифкосмайлы"
+  }
+]);
