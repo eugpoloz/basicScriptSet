2277580bac854445a7afe22775bc652125b18c21
Evgenia Polozova <eugpoloz@gmail.com>
2018-05-10 21:30:21 +0300

    bss default icon fixes

--- src/bss.js
+++ src/bss.js
@@ -20,7 +20,7 @@ declare var $: any;
 
 type DefaultIcon = {
   icon?: string,
-  after?: string
+  [position: "after" | "before"]: string | null
 } | null;
 
 type FastLogin = {
@@ -131,23 +131,34 @@ function basicScriptSet({ disabledProfiles, defaultIcon, fastLogin }: Options) {
       return;
     }
 
-    const { icon = "", after = ".pa-title" } = defaultIcon;
+    // мы предполагаем, что если не передается вообще никаких параметров,
+    // то вставлять аватарку надо всегда ПОСЛЕ статуса .pa-title
+    const { icon = "", after = ".pa-title", before = null } = defaultIcon;
 
+    // если нет урла, то все понятно
     if (icon === "") {
       return;
     }
 
+    // если есть урл, см. строку 136 и комментарий
     if (typeof FORUM.topic === "object") {
       document.querySelectorAll(".post-author ul").forEach(author => {
         if (author.querySelector(".pa-avatar")) return;
-
-        const el = after && author.querySelector(after);
         const authorLink = author.querySelector(".pa-author a");
         const alt = (authorLink && authorLink.textContent) || "guest";
         const html = `<li class="pa-avatar item2 default-icon"><img src="${icon}" alt="${alt}" style="cursor: pointer;"></li>`;
 
-        if (el) {
-          return el.insertAdjacentHTML("afterend", html);
+        // сначала мы чекаем, есть ли у нас before, и отдаем его
+        if (before) {
+          const beforeEl = author.querySelector(before);
+          return beforeEl && beforeEl.insertAdjacentHTML("beforebegin", html);
+        }
+
+        // потом мы чекаем, есть ли у нас after;
+        // если и его нет, то подставится наш дефолтный ПОСЛЕ .pa-title
+        if (after) {
+          const afterEl = author.querySelector(after);
+          return afterEl && afterEl.insertAdjacentHTML("afterend", html);
         }
       });
     }
