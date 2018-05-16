14d28c872c0df87e337b3105af28ab8a13575a9f
Evgenia Polozova <eugpoloz@gmail.com>
2018-05-16 03:09:31 +0300

    more fixes and some console.log()s to charCounter

--- src/modules/charCounter.js
+++ src/modules/charCounter.js
@@ -11,18 +11,23 @@ export default function charCounter() {
 
   if (typeof FORUM.editor === "object") {
     const textarea = document.getElementById("main-reply");
-    const counter = document.querySelector("#charcounter .charcount");
     const counterSibling = document.querySelector("#post p.areafield.required");
 
     function updateCharCounter({ currentTarget }: Event) {
+      console.log("ran");
       if (currentTarget instanceof HTMLTextAreaElement) {
         const { length } = currentTarget.value;
 
+        console.log(length);
+
+        const counter = document.querySelector("#charcounter .charcount");
         if (counter) {
           counter.innerText = length.toString();
         }
       }
     }
+
+    const debouncedUpdate = debounce(e => updateCharCounter(e), 200);
 
     if (textarea) {
       if (counterSibling) {
@@ -30,7 +35,7 @@ export default function charCounter() {
       }
       textarea.addEventListener(
         "keypress keydown keyup change cut copy paste",
-        e => debounce(updateCharCounter(e), 200)
+        debouncedUpdate
       );
     }
   }
