24088c38c94d32314b9a268991531d4592d8c4d5
Evgenia <eugpoloz@gmail.com>
2018-05-18 16:59:49 +0300

    switch document for window

--- src/modules/selectAndCopy.js
+++ src/modules/selectAndCopy.js
@@ -105,7 +105,7 @@ export default function selectCodeBoxContents(props: CodeBoxProps) {
 export function listenToCodeBox() {
   const codeboxNodeList = document.querySelectorAll(".code-box");
 
-  document.addEventListener("load", () => {
+  window.addEventListener("load", () => {
     function codeSelector(e: MouseEvent) {
       console.log(e);
     }
