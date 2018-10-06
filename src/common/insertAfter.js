0b2581e15d554d4f848cb5ce023895a524f9a2f6
Evgenia Polozova <eugpoloz@gmail.com>
2018-10-06 20:25:54 +0300

    add a line break

--- src/common/insertAfter.js
+++ src/common/insertAfter.js
@@ -1,6 +1,7 @@
 // @flow
 export default function insertAfter(query: string, html: string) {
   const el = document.querySelector(query);
+
   if (el) {
     el.insertAdjacentHTML("afterend", html);
   }
