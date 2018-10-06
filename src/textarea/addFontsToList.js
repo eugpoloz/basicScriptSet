210e0c436c811e5bd2402c2958b18d5c027ce4fe
Evgenia Polozova <eugpoloz@gmail.com>
2018-10-06 20:22:02 +0300

    tweak addFontsToList func

--- src/textarea/addFontsToList.js
+++ src/textarea/addFontsToList.js
@@ -1,15 +1,20 @@
 // @flow
 declare var FORUM: Object;
 
+const fontHMTL = (font: string) =>
+  `<div style="font-family:${font}">
+    <span>${font}</span> <img onclick="bbcode('[font=${font}]','[/font]')" src="/i/blank.gif">
+  </div>`;
+
 export default function addFontsToList(fonts: Array<string>) {
   if (typeof FORUM.editor === "object") {
     const fontArea = document.getElementById("font-area");
-    if (fontArea) {
-      fonts.map(font => {
-        const html = `<div style="font-family:${font}"><span>${font}</span> <img onclick="bbcode('[font=${font}]','[/font]')" src="/i/blank.gif"> </div>`;
 
-        fontArea.insertAdjacentHTML("beforeend", html);
-      });
+    if (fontArea instanceof HTMLElement) {
+      const html = fonts.map(fontHMTL).join("");
+
+      fontArea.insertAdjacentHTML("beforeend", html);
+      return;
     }
   }
 }
