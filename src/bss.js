2fc93b578404d5b33a5f80b90e7db2e96b3dd953
Evgenia Polozova <eugpoloz@gmail.com>
2018-10-06 15:34:44 +0300

    split modules as intended

--- src/bss.js
+++ src/bss.js
@@ -11,37 +11,31 @@
   — http://urchoice.rolka.su/profile.php?id=4789
 */
 
-import setDefaultIcon from "./topic/setDefaultIcon";
-import createFastLoginLinks from "./various/createFastLoginLinks";
 import addFontsToList from "./textarea/addFontsToList";
 import charCounter from "./textarea/charCounter";
-import selectCodeBox from "./topic/selectAndCopy";
 import fastSubmit from "./textarea/fastSubmit";
 import {
   addCtrlClicks,
   originalUploadedFirst
 } from "./textarea/refactorEditorButtons";
-import disableProfiles from "./various/disableProfiles";
+
+import setDefaultIcon from "./topic/setDefaultIcon";
+import selectCodeBox from "./topic/selectAndCopy";
 import clickableGuestNames from "./topic/clickableGuestNames";
 
-import type { DefaultIcon, FastLogin } from "./commonTypes";
-
-type Options = {
-  defaultIcon: DefaultIcon,
-  fastLogin: FastLogin
-};
+import createFastLoginLinks from "./various/createFastLoginLinks";
+import disableProfiles from "./various/disableProfiles";
 
 // basic function
-export function bss({ defaultIcon, fastLogin }: Options) {
+export function enhanceTextarea() {
   addCtrlClicks();
   originalUploadedFirst();
-
-  // calling functions w/ passed props
-  if (defaultIcon) setDefaultIcon(defaultIcon);
-  if (fastLogin) createFastLoginLinks(fastLogin);
 }
 
+// module exports
 export {
+  setDefaultIcon,
+  createFastLoginLinks,
   addFontsToList,
   charCounter,
   selectCodeBox,
@@ -51,21 +45,10 @@ export {
 };
 
 // possible config for reference:
-// basicScriptSet({
-//   disabledProfiles: {
-//     profiles: [4]
-//   }, // айди профилей в квадратных скобках через запятую
-//   defaultIcon: {
-//     icon: "http://forumavatars.ru/img/avatars/0019/83/8b/85-1520334341.png" // ссылка
-//   },
-//   fastLogin: {
-//     logins: [
-//       {
-//         login: "reader test",
-//         password: "12345",
-//         id: "navreader",
-//         link: "Peek Inside"
-//       }
-//     ]
-//   }
+
+// basicScriptSet.enhanceTextarea();
+// basicScriptSet.setDefaultIcon("http://forumavatars.ru/img/avatars/0019/83/8b/85-1520334341.png" /* ссылка */ );
+// basicScriptSet.disableProfiles({
+//   profiles: [4],
+//   message: "Don't even think about it!"
 // });
