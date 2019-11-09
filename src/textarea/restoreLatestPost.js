73ed3417d87767cfc7fd1f28608ed45202b47127
Evgenia Polozova <eugpoloz@gmail.com>
2019-11-09 19:49:55 +0300

    fix restoreLatestPost implementation

--- src/textarea/restoreLatestPost.js
+++ src/textarea/restoreLatestPost.js
@@ -1,21 +1,37 @@
+// @flow
+function getLatestPost(): string {
+  let reservedPost: string = "";
+
+  // todo: enhance this w/ topic key getter
+  try {
+    reservedPost = window.localStorage.getItem("ReservePost");
+  } catch (error) {
+    console.error("Can't get localStorage.ReservePost", error);
+  }
+
+  return reservedPost;
+}
+
 function restorePost() {
   const mainReply = document.getElementById("main-reply");
 
   if (mainReply instanceof HTMLTextAreaElement) {
-    mainReply.value = localStorage.ReservePost;
+    mainReply.value = getLatestPost();
   }
 }
 
 function addRestorePostButton() {
   const additionArea = document.getElementById("addition-area");
 
-  const restoreBtn = `<div id="reservedPostRestorer" role="button"><span>Восстановить последний пост</span></div>`;
+  const restoreBtn = `<div class="js-post-restorer"><span>Восстановить последний пост</span></div>`;
 
   if (additionArea) {
     additionArea.insertAdjacentHTML("beforeend", restoreBtn);
-    const postRestorer = document.getElementById("reservedPostRestorer");
+    const postRestorer = document.querySelector(".js-post-restorer");
 
-    reservedPostRestorer.addEventListener("click", restorePost);
+    if (postRestorer) {
+      postRestorer.addEventListener("click", restorePost);
+    }
   }
 }
 
