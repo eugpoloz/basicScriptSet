210cda4898633f31baf4f17c92de03c5b9b1206d
Evgenia Polozova <eugpoloz@gmail.com>
2025-03-02 15:53:00 +0300

    fix(disableProfiles): remove edit link

--- src/various/disableProfiles.js
+++ src/various/disableProfiles.js
@@ -3,7 +3,11 @@ export default function disableProfiles({
   message = "Редактирование данного профиля для вас запрещено!"
 }) {
   if (profiles.length > 0) {
-    const innerHTML = `<p style="margin: 1em 0; line-height: 2; text-align: center;">${message}</p>`;
+    const editLink = document.querySelector("#viewprofile #pa-edit");
+
+    if (profiles.includes(UserID) && editLink) {
+      editLink.remove();
+    }
 
     // оказывается, у нас есть глобальная переменная profile,
     // которая функция на всех страницах, кроме профиля,
@@ -12,6 +16,8 @@ export default function disableProfiles({
       profile.style.display = "none";
 
       if (profiles.includes(UserID)) {
+        const innerHTML = `<p style="margin: 1em 0; line-height: 2; text-align: center;">${message}</p>`;
+
         profile.innerHTML = innerHTML;
       }
 
