858cca7143aa0d294fdf9e7865692804cb5f281d
Evgenia Polozova <eugpoloz@gmail.com>
2025-04-10 21:25:22 +0300

    fix(charCounter,fastSubmit): remove insertAfter, fix styles & targets

--- src/common/index.js
+++
@@ -1,3 +0,0 @@
-import insertAfter from "./insertAfter";
-
-export { insertAfter };
--- src/common/insertAfter.js
+++
@@ -1,7 +0,0 @@
-export default function insertAfter(query, html) {
-  const el = document.querySelector(query);
-
-  if (el) {
-    el.insertAdjacentHTML("afterend", html);
-  }
-}
